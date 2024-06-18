e1494d92efeb55c4ecca7251c7f63f0078a42397
FredericoNicola <fredericonicola@gmail.com>
2024-06-18 16:52:18 +0100

    test

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -3,10 +3,12 @@ import { openBrowserAsync } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
 
+
 type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
 
 export function ExternalLink({ href, ...rest }: Props) {
   return (
+    
     <Link
       target="_blank"
       {...rest}
